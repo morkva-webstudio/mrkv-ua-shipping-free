94f8c036a2ade46b2be531c8b7ab3b16b8fe2efd
unknown <info.dpmine@gmail.com>
2026-09-18 09:08:26 +0300

    1.12.3 https://trello.morkva.co.ua/cards/oknb7f0ka1e0 fixed shipping rate value lable - Egor

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: bandido, dpmine
 Tags: Nova Poshta, Нова Пошта, Укрпошта
 Tested up to: 7.1
 Requires PHP: 7.4
-Stable tag: 1.12.2
+Stable tag: 1.12.3
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -176,6 +176,9 @@ https://youtu.be/47-VrH_QJAE
 
 
 == Changelog ==
+
+= 1.12.3 =
+* [new] додали вивід напису за тарифами перевізника у оновленому шаблоні email Woocommerce 
 
 = 1.12.2 =
 * [hotfix] виправили помилку класу Rozetka
--- classes/mrkv-ua-shipping-connecter.php
+++ classes/mrkv-ua-shipping-connecter.php
@@ -15,6 +15,7 @@ if (!class_exists('MRKV_UA_SHIPPING_CONNECTER'))
 		function __construct()
 		{
             add_filter( 'woocommerce_shipping_methods', [$this, 'mrkv_ua_shipping_add_shipping_method_woo'] );
+            add_filter( 'woocommerce_order_shipping_to_display', [$this, 'mrkv_ua_shipping_shipping_display_text'], 10, 3 );
 
             new MRKV_UA_SHIPPING_BLOCKS();
 		}
@@ -46,6 +47,55 @@ if (!class_exists('MRKV_UA_SHIPPING_CONNECTER'))
 
             # Return all methods
             return $methods;
+        }
+
+        /**
+         * Changing the shipping cost text when the price is zero
+         * 
+         * @param string $shipping
+         * @param WC_Order $order
+         * @param string $tax_display
+         * 
+         * @return string
+         */
+        public function mrkv_ua_shipping_shipping_display_text( $shipping, $order, $tax_display )
+        {
+            if ( (float) $order->get_shipping_total() !== 0.0 ) {
+                return $shipping;
+            }
+
+            foreach ( $order->get_shipping_methods() as $shipping_item ) {
+                $method_id = $shipping_item->get_method_id();
+
+                if ( strpos( $method_id, 'mrkv_ua_shipping_' ) !== false ) {
+                    $wc_shipping = WC_Shipping::instance();
+                    $all_methods = $wc_shipping->get_shipping_methods();
+
+                    if ( isset( $all_methods[ $method_id ] ) ) {
+                        $shipping_class = $all_methods[ $method_id ];
+                        $shipping_instance = is_string( $shipping_class ) ? new $shipping_class( $shipping_item->get_instance_id() ) : $shipping_class;
+
+                        if ( 'yes' === $shipping_instance->get_option( 'enable_minimum_cost' ) ) {
+                            $woo_cart_total = $order->get_subtotal();
+                            
+                            $settings_method = get_option('ukr-poshta_m_ua_settings');
+                            if ( isset($settings_method['shipment']['cart_total']) && $settings_method['shipment']['cart_total'] == 'total' ) {
+                                $woo_cart_total = $order->get_total() - $order->get_shipping_total();
+                            }
+
+                            $min_cost_total = (float) $shipping_instance->get_option( 'minimum_cost_total' );
+
+                            if ( $woo_cart_total >= $min_cost_total ) {
+                                return __( 'Free', 'mrkv-ua-shipping' );
+                            }
+                        }
+
+                        return __( 'According to carrier rates', 'mrkv-ua-shipping' );
+                    }
+                }
+            }
+
+            return $shipping;
         }
 	}
 }
